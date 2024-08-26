57b089bbe87020ee8205597b97c0b6fcf62a278b
Eetu Eskelinen <eetuesk@gmail.com>
2024-08-27 00:57:42 +0300

    add basic table

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
-import { Link } from 'react-router-dom';
 import { useUser } from '../context/AuthContext';
 import { auth } from '../firebase/firebase';
 import { SignIn } from '@/components/SignIn';
 import { SignUp } from '@/components/SignUp';
+import List from '@/components/List';
 import { useState } from 'react';
 
 const HomePage = () => {
@@ -10,62 +10,35 @@ const HomePage = () => {
   const [isSignIn, setIsSignIn] = useState(true);
 
   return (
-    <main>
-      <section className="main-container">
-        <h1 className="header-text">React Firebase Auth Template</h1>
-        <p>Current User : {user?.email || 'None'}</p>
-
-        {user && <button onClick={() => auth.signOut()}>Sign Out</button>}
-
-        {!user && (
-          <>
-            {/* Toggle between sign in and sign up */}
-            <button
-              onClick={() => {
-                setIsSignIn((prev) => !prev);
-              }}
-            >
-              {isSignIn ? 'Sign Up' : 'Sign In'}
-            </button>
-
-            {isSignIn ? <SignIn /> : <SignUp />}
-          </>
-        )}
-      </section>
-    </main>
-    // <main>
-    //   <section className="main-container">
-    //     <h1 className="header-text">React Firebase Auth Template</h1>
-    //     <p>Current User : {user?.email || 'None'}</p>
-    //     {user ? (
-    //       <button onClick={() => auth.signOut()}>Sign Out</button>
-    //     ) : (
-    //       <Link to="/auth/sign-in">Sign In</Link>
-    //     )}
-    //     <Link to="/protected">Protected Page 🛡️</Link>
-    //     <div id="divider"></div>
-    //     <Link
-    //       to="https://github.com/mmvergara/react-firebase-auth-template"
-    //       target="_blank"
-    //       rel="noreferrer noopener"
-    //       id="github-repo-link"
-    //     >
-    //       <svg
-    //         xmlns="http://www.w3.org/2000/svg"
-    //         x="0px"
-    //         y="0px"
-    //         width="30"
-    //         height="50"
-    //         viewBox="0 0 64 64"
-    //       >
-    //         <path
-    //           fill="#fff"
-    //           d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z"
-    //         ></path>
-    //       </svg>
-    //       Github Repository
-    //     </Link>
+    <div className="flex flex-col h-screen">
+      <header className="p-4 bg-gray-800 text-white">Header</header>
+      <main className="flex-1 overflow-auto">
+        <List />
+      </main>
+    </div>
+    // <main className="mx-auto max-w-screen-xl flex flex-col max-h-screen px-4 pt-10 sm:px-6 sm:pt-24 lg:px-8">
+    //   <section className="px-4 sm:px-6 lg:px-8 mb-6">
+    //     <div className="flex flex-col justify-between flex-shrink-0">
+    //       <h1 className="block text-2xl font-bold sm:text-4xl flex-shrink">
+    //         Ostoslista
+    //       </h1>
+    //       <p>Current User : {user?.email || 'None'}</p>
+    //     </div>
+    //     {user && <button onClick={() => auth.signOut()}>Sign Out</button>}
     //   </section>
+
+    //   {user ? (
+    //     <section className="flex-1 w-full bg-slate-700">
+    //       <List />
+    //     </section>
+    //   ) : (
+    //     <>
+    //       <button onClick={() => setIsSignIn((prev) => !prev)} className="mb-4">
+    //         {isSignIn ? 'Sign Up' : 'Sign In'}
+    //       </button>
+    //       {isSignIn ? <SignIn /> : <SignUp />}
+    //     </>
+    //   )}
     // </main>
   );
 };
